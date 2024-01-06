0721b6d228cb9e66a00fc9a663a0039386815e27
Pawel <pawel.mirys@gmail.com>
2024-01-06 15:01:04 +0100

    footer layout fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,27 +4,22 @@ const Footer = () => {
   return (
     <footer
       className={clsx(
-        'flex flex-row gap-1 justify-center items-center border-t-2 h-20 mt-5'
+        'flex items-center flex-wrap gap-1 justify-center border-t-2 h-20 mt-5 text-sm mx-4'
       )}>
-      <div>
-        <span> The project is based on</span>
-        <a
-          className='text-sky-700 ml-1'
-          target='_blank'
-          href='https://demo.realworld.io/#/'>
-          RealWorld
-        </a>
-      </div>
-      &
-      <div>
-        <span>RealWorld</span>
-        <a
-          className='text-sky-700 ml-1'
-          target='_blank'
-          href='https://realworld-docs.netlify.app/docs/intro'>
-          Docs
-        </a>
-      </div>
+      <span> The project is based on</span>
+      <a
+        className='text-sky-700 '
+        target='_blank'
+        href='https://demo.realworld.io/#/'>
+        RealWorld
+      </a>
+      &<span>RealWorld</span>
+      <a
+        className='text-sky-700 '
+        target='_blank'
+        href='https://realworld-docs.netlify.app/docs/intro'>
+        Docs
+      </a>
     </footer>
   );
 };
